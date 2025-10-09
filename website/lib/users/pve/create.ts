de46975d977bc94211c14f3fbe9661d62a38f403
Alexander Taylor <52684858+awtpi314@users.noreply.github.com>
2025-10-09 18:40:42 -0400

    changes to admin page

--- website/lib/users/pve/create.ts
+++ website/lib/users/pve/create.ts
@@ -1,32 +1,36 @@
 "use server";
 
-import { proxmoxApi } from "proxmox-api";
+import { proxmox } from "@/proxmox";
+import { revalidatePath } from "next/cache";
 
 export async function createPVEUser(
-  userId: string,
-  initialAccess: Array<string>
+  pveUserId: string,
+  initialAccess: Array<string>,
+  dbUserId?: number
 ) {
-  const proxmox = proxmoxApi({
-    host: process.env.PVE_HOST || "pve.jericho.local",
-    port: process.env.PVE_PORT ? parseInt(process.env.PVE_PORT) : 8006,
-    username: process.env.PVE_USER || "root@pam",
-    password: process.env.PVE_PASSWORD || "password",
-  });
-
   await proxmox.access.users.$post({
-    userid: `${userId}@pve`,
+    userid: `${pveUserId}@pve`,
     password: process.env.PVE_CREATE_USER_PASSWORD || "DefaultPassword123!",
     enable: true,
   });
+
+  if (dbUserId) {
+    await prisma?.user.update({
+      where: { id: dbUserId },
+      data: { proxmoxId: pveUserId },
+    });
+  }
 
   await Promise.all(
     initialAccess.map((path) =>
       proxmox.access.acl.$put({
         path,
         roles: "PVEVMUser",
-        users: `${userId}@pve`,
+        users: `${pveUserId}@pve`,
         propagate: true,
       })
     )
   );
+
+  revalidatePath("/admin");
 }
---
+++ website/lib/users/pve/delete.ts
@@ -0,0 +1,13 @@
+"use server";
+
+import { proxmox } from "@/proxmox";
+import { revalidatePath } from "next/cache";
+
+export async function deletePVEUser(pveUserId: string) {
+  await proxmox.access.users.$(`${pveUserId}@pve`).$delete();
+  await prisma?.user.updateMany({
+    where: { proxmoxId: pveUserId },
+    data: { proxmoxId: null },
+  });
+  revalidatePath("/admin");
+}
