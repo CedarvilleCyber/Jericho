69f74a112d9778ec4bf69752ed7a0ac9eb1b08d4
David <davidmoore155@cedarville.edu>
2025-10-16 16:17:57 -0400

    files I forgot to add

---
+++ 3-water-treatment-scenario/topnav.php
@@ -0,0 +1,16 @@
+<head>
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/global.css">
+</head>
+<div class="topnav">
+    <nav class="navbar fixed-top navbar-expand-md navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"><img src=./images/logo.svg class=logo></a>
+            <div class="navbar-header">
+                <a class="navbar-brand mx-auto" href="#">Jericho Water</a>
+            </div>
+            <a href="./logout.php" class="ms-auto">Log Out</a>
+            <a href="./admin.php" class="ms-auto">Admin Page</a>
+        </div>
+    </nav>
+</div>
