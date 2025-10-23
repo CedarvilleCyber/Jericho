d9a01b7c7dcd57517bbe315817d05f9161361dbc
David <davidmoore155@cedarville.edu>
2025-10-23 18:05:43 -0400

    renamed stuff

---
+++ 3-water-scenario/month.php
@@ -0,0 +1,8 @@
+<?php
+    session_start();
+
+    // Use CSS Grid to display the employees so you can have two central columns.
+    // Using CSS Grid means that when you add a new employee, he or she will always
+    // be prominently located in the top left square, which is what you want.
+    // That would be harder to do in flexbox.
+?>
