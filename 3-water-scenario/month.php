64c56cff12c94055ba40e48837a8502d22e36fca
Alexander Taylor <52684858+awtpi314@users.noreply.github.com>
2025-10-29 00:30:26 -0400

    styling for the website

--- 3-water-scenario/month.php
+++ 3-water-scenario/month.php
@@ -1,34 +1,59 @@
 <?php
-    session_start();
+session_start();
 
-    $date = (new DateTime())->modify('-1 month'); // set to previous month
-    $employees = ["Michael Rembrandt", "Redd Harlan", "Lauren Towner", 
-                  "Carla Winslow", "Hugh Farnham", "Anthony Smith", 
-                  "Erik Williams", "Clare Sidney", "Emma Chavez",
-                  "Annie Gregory", "Suzanne Bender", "Kevin Mitnick"];
+$date = (new DateTime())->modify('-1 month'); // set to previous month
+$employees = [
+  "Michael Rembrandt",
+  "Redd Harlan",
+  "Lauren Towner",
+  "Carla Winslow",
+  "Hugh Farnham",
+  "Anthony Smith",
+  "Erik Williams",
+  "Clare Sidney",
+  "Emma Chavez",
+  "Annie Gregory",
+  "Suzanne Bender",
+  "Kevin Mitnick"
+];
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title>Employee of the Month</title>
-        <?php include 'topnav.php'; ?> 
-        <link rel="stylesheet" href="./css/month.css">
-    </head>
-    <p>Here, we honor our greatest employees.</p>
-    <div class="grid-container">
-        <?php
-            for ($i = 0; $i < 12; $i++) {
-                echo '<div class="box">';
 
-                // Crop the images so they're all the same size.
-                printf("<img src='./images/employees/%d.jpg' width='200'>", $i);
+<head>
+  <title>Employee of the Month</title>
+  <?php include 'include.php'; ?>
+  <link rel="stylesheet" href="/css/month.css">
+</head>
 
-                printf("<p>%s</p>", $employees[$i]);
-                printf("<p>%s</p>", $date->format('F Y'));
-                echo '</div>';
-                $date->modify('-1 month');
-            }
-        ?>
+<body>
+  <?php include 'topnav.php'; ?>
+  <div class="flex-container">
+    <div class="snap-container">
+      <?php
+      for ($i = 0; $i < 12; $i++) {
+        echo '<section class="slide" aria-label="Employee of the Month">';
+
+        echo '<h1 class="page-title">Employee of the Month</h1>';
+
+        printf(
+          "<img class='employee-photo' src='./images/employees/%d.jpg' alt='%s' loading='lazy'>",
+          $i,
+          htmlspecialchars($employees[$i], ENT_QUOTES)
+        );
+
+        printf("<p class='employee-name'>%s</p>", htmlspecialchars($employees[$i], ENT_QUOTES));
+        printf("<p class='employee-date'>%s</p>", $date->format('F Y'));
+        if ($i < 11) {
+          echo "<div class='scroll-hint' aria-hidden='true'>Scroll for more &#8595;</div>";
+        }
+        echo '</section>';
+        $date->modify('-1 month');
+      }
+      ?>
     </div>
+  </div>
+</body>
+
 </html>
--- 3-water-scenario/topnav.php
+++ 3-water-scenario/topnav.php
@@ -1,39 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title>Jericho</title>
-        <link rel="stylesheet" href="./css/bootstrap.min.css">
-        <link rel="stylesheet" href="./css/global.css">
-    </head>
-    <body>
-        <div class="container">
-            <header class="d-flex flex-wrap justify-content-center py-3">
-                <a class="d-flex align-items-center me-md-auto page-title" href="#">
-                    <img src="./images/logo.svg" alt="Jericho Logo" class="logo">
-                    <span class="ms-2">Jericho Water</span>
-                </a>
-                <ul class="nav nav-pills">
-                    <li class="nav-item"><a href="./index.php" class="nav-link active" aria-current="page">Home</a></li>
-                    <li class="nav-item"><a href="./admin.php" class="nav-link">Admin</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link">About</a></li>
-                    <li class="nav-item"><a href="./month.php" class="nav-link">Employee of the Month</a></li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" 
-                        role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Account
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="./login.php">Log In</a></li>
-                            <li><a class="dropdown-item" href="./logout.php">Log Out</a></li>
-                        </ul>
-                    </li>
-                </ul>
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+  session_start();
+}
+$loggedIn = !empty($_SESSION['user']);
+?>
+<header class="d-flex flex-wrap justify-content-center py-3 px-3 bg-dark"
+  style="position:sticky; top: 0; z-index: 100; height: 4.5rem;">
+  <a class="d-flex align-items-center me-md-auto page-title" href="/">
+    <img src="./images/logo.svg" alt="Jericho Logo" class="logo">
+    <span class="ms-2">Jericho Water</span>
+  </a>
 
-            <!-- TODO: bugfix. For some reason, when you're on the admin page, 
-            the nav bar is narrower than when you're on the index page. -->
+  <ul class="nav nav-pills">
+    <?php if ($loggedIn): ?>
+      <li class="nav-item"><a href="./admin.php" class="nav-link">Admin</a></li>
+      <li class="nav-item"><a href="./month.php" class="nav-link">Employee of the Month</a></li>
 
-            </header>
-        </div>
-        <script src="./js/bootstrap.bundle.min.js"></script>
-    </body>
-</html>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">
+          <?php echo htmlspecialchars($_SESSION['user']['name'] ?? $_SESSION['user']); ?>
+        </a>
+        <ul class="dropdown-menu dropdown-menu-dark dropdown-menu-end">
+          <li><a class="dropdown-item" href="./logout.php">Log Out</a></li>
+        </ul>
+      </li>
+    <?php else: ?>
+      <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+      <li class="nav-item"><a href="./login.php" class="nav-link">Log In</a></li>
+    <?php endif; ?>
+  </ul>
+</header>
