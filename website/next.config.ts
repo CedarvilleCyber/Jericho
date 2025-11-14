354899402f609024317804a75b07d8227aa3744f
Alexander Taylor <52684858+awtpi314@users.noreply.github.com>
2025-11-14 20:45:03 +0000

    fixed next config

--- website/next.config.ts
+++ website/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: "/webapp",
-  allowedDevOrigins: ["https://jericho.alexthetaylor.com"],
+  allowedDevOrigins: ["jericho.alexthetaylor.com"],
 };
 
 export default nextConfig;
