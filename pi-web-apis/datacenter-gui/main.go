fecaf7dc6544198a88d0d67bc8743e663020d9dd
Major-Q <63882792+Major-Q@users.noreply.github.com>
2026-06-30 15:23:09 -0400

    Basic Layout
    Setup up a basic layout

--- pi-web-apis/datacenter-gui/main.go
+++ pi-web-apis/datacenter-gui/main.go
@@ -6,14 +6,17 @@ import (
 	"os"
 
 	"gioui.org/app"
+	"gioui.org/layout"
 	"gioui.org/op"
 	"gioui.org/op/paint"
+	"gioui.org/widget"
 	"gioui.org/widget/material"
 )
 
 func main() {
 	go func() {
 		w := new(app.Window)
+		w.Option(app.Title("Datacenter GUI"))
 		if err := run(w); err != nil {
 			log.Fatal(err)
 		}
@@ -26,6 +29,7 @@ func main() {
 func run(w *app.Window) error {
 	th := material.NewTheme()
 	var ops op.Ops
+	var btn1, btn2, btn3, btn4, btn5, btn6 widget.Clickable
 	for {
 		switch e := w.Event().(type) {
 		case app.DestroyEvent:
@@ -33,9 +37,46 @@ func run(w *app.Window) error {
 		case app.FrameEvent:
 			gtx := app.NewContext(&ops, e)
 			paint.Fill(gtx.Ops, color.NRGBA{R: 20, G: 20, B: 20, A: 255})
-			lbl := material.H1(th, "Hello, World!")
-			lbl.Color = color.NRGBA{R: 255, G: 255, B: 255, A: 255}
-			lbl.Layout(gtx)
+
+			layout.Flex{
+				Axis: layout.Vertical,
+			}.Layout(gtx,
+				layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
+					return layout.Flex{
+						Axis: layout.Horizontal,
+					}.Layout(gtx,
+						layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
+							return material.Button(th, &btn1, "Button1").Layout(gtx)
+						}),
+						layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
+							return material.Button(th, &btn2, "Button2").Layout(gtx)
+						}),
+					)
+				}),
+				layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
+					return layout.Flex{
+						Axis: layout.Horizontal,
+					}.Layout(gtx,
+						layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
+							return material.Button(th, &btn3, "Button3").Layout(gtx)
+						}),
+						layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
+							return material.Button(th, &btn4, "Button4").Layout(gtx)
+						}),
+					)
+				}), layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
+					return layout.Flex{
+						Axis: layout.Horizontal,
+					}.Layout(gtx,
+						layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
+							return material.Button(th, &btn5, "Button5").Layout(gtx)
+						}),
+						layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
+							return material.Button(th, &btn6, "Button6").Layout(gtx)
+						}),
+					)
+				}),
+			)
 			e.Frame(gtx.Ops)
 		}
 
