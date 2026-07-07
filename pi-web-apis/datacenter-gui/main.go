a69457af36b8b3db2f95a46f46274e78fefd6f38
Major-Q <63882792+Major-Q@users.noreply.github.com>
2026-07-07 12:26:04 -0400

    Add Basic Trigger Dashboard
    Created a basic trigger dashboard that currently only tests if the APIs are available

--- pi-web-apis/datacenter-gui/main.go
+++ pi-web-apis/datacenter-gui/main.go
@@ -1,22 +1,36 @@
 package main
 
 import (
+	"fmt"
+	"image"
 	"image/color"
+	"io"
 	"log"
+	"net/http"
 	"os"
+	"time"
 
 	"gioui.org/app"
 	"gioui.org/layout"
 	"gioui.org/op"
+	"gioui.org/op/clip"
 	"gioui.org/op/paint"
+	"gioui.org/widget"
 	"gioui.org/widget/material"
 )
+
+type Trigger struct {
+	Name   string
+	URL    string
+	Color  color.NRGBA
+	Button widget.Clickable
+}
 
 func main() {
 	go func() {
 		w := new(app.Window)
 		w.Option(app.Title("Datacenter GUI"))
-		if err := run(w); err != nil {
+		if err := triggers(w); err != nil {
 			log.Fatal(err)
 		}
 		os.Exit(0)
@@ -25,7 +39,130 @@ func main() {
 	app.Main()
 }
 
-func run(w *app.Window) error {
+func triggers(w *app.Window) error {
+	th := material.NewTheme()
+	var ops op.Ops
+	var triggers [4]Trigger
+	triggers[0] = Trigger{Name: "Nuclear", URL: "http://nuclear.jericho.local/health", Color: color.NRGBA{R: 255, A: 255}}
+	triggers[1] = Trigger{Name: "Traffic", URL: "http://traffic.jericho.local/health", Color: color.NRGBA{G: 255, B: 120, R: 100, A: 255}}
+	triggers[2] = Trigger{Name: "Water", URL: "http://water.jericho.local/health", Color: color.NRGBA{B: 255, A: 255}}
+	triggers[3] = Trigger{Name: "Sound", URL: "http://sound.jericho.local/health", Color: color.NRGBA{R: 0, G: 0, B: 0, A: 255}}
+	client := http.Client{
+		Timeout: 10 * time.Second,
+	}
+	for {
+		switch e := w.Event().(type) {
+		case app.DestroyEvent:
+			return e.Err
+		case app.FrameEvent:
+			gtx := app.NewContext(&ops, e)
+
+			for i := range triggers {
+				if triggers[i].Button.Clicked(gtx) {
+					trigger := &triggers[i]
+					fmt.Printf("Trigger: %s Clicked\n", trigger.Name)
+					go func(t *Trigger) {
+						resp, err := client.Get(t.URL)
+						if err != nil {
+							fmt.Printf("%s error: %v\n", t.Name, err)
+							return
+						}
+						defer func(Body io.ReadCloser) {
+							err := Body.Close()
+							if err != nil {
+								fmt.Printf("%s error: %v\n", t.Name, err)
+								return
+							}
+						}(resp.Body)
+
+						fmt.Printf("%s response: %s\n", t.Name, resp.Status)
+					}(trigger)
+				}
+			}
+
+			layout.Flex{
+				Axis: layout.Vertical,
+			}.Layout(gtx,
+				layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
+					return layout.Flex{Axis: layout.Horizontal}.Layout(gtx,
+						layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
+							btn := material.Button(th, &triggers[0].Button, triggers[0].Name)
+							btn.Background = triggers[0].Color
+
+							return btn.Layout(gtx)
+						}),
+						layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
+							btn := material.Button(th, &triggers[1].Button, triggers[1].Name)
+							btn.Background = triggers[1].Color
+							return btn.Layout(gtx)
+						}),
+					)
+
+				}),
+				layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
+					return layout.Flex{Axis: layout.Horizontal}.Layout(gtx,
+						layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
+							btn := material.Button(th, &triggers[2].Button, triggers[2].Name)
+							btn.Background = triggers[2].Color
+							return btn.Layout(gtx)
+						}),
+						layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
+							btn := material.Button(th, &triggers[3].Button, triggers[3].Name)
+							btn.Background = triggers[3].Color
+							return btn.Layout(gtx)
+						}),
+					)
+
+				}),
+			)
+			e.Frame(gtx.Ops)
+
+		}
+
+	}
+}
+
+func rotatingCoin(w *app.Window) error {
+	//th := material.NewTheme()
+	var ops op.Ops
+	swap := false
+
+	go func() {
+		ticker := time.NewTicker(5 * time.Second)
+		defer ticker.Stop()
+
+		for range ticker.C {
+			swap = !swap
+
+			w.Invalidate()
+		}
+	}()
+
+	for {
+		switch e := w.Event().(type) {
+		case app.DestroyEvent:
+			return e.Err
+		case app.FrameEvent:
+			gtx := app.NewContext(&ops, e)
+
+			currentColor := color.NRGBA{A: 255}
+			if swap {
+				currentColor = color.NRGBA{G: 100, A: 255}
+			}
+
+			circle := clip.Ellipse{Min: image.Pt(100, 100), Max: image.Pt(300, 300)}.Push(gtx.Ops)
+
+			paint.Fill(gtx.Ops, currentColor)
+
+			circle.Pop()
+
+			e.Frame(gtx.Ops)
+		}
+
+	}
+}
+
+func jerichoDisplay(w *app.Window) error {
 	th := material.NewTheme()
 	var ops op.Ops
 	for {
