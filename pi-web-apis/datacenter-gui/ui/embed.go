30c85b1dc3de1beca6b64ee2916df9743d5ef215
Major-Q <63882792+Major-Q@users.noreply.github.com>
2026-07-17 15:21:27 -0400

    Add coin
    Added a coin with the Jericho logo that spins

--- pi-web-apis/datacenter-gui/ui/embed.go
+++ pi-web-apis/datacenter-gui/ui/embed.go
@@ -5,5 +5,5 @@ import "embed"
 // Files holds all UI assets (HTML, CSS, JS) embedded into the binary at compile time.
 // Adding new files to the ui/css or ui/js directories will automatically include them.
 //
-//go:embed index.html css js
+//go:embed index.html css js images
 var Files embed.FS
